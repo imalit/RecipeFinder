da0dc951f2fca5b88bcceb66aefe2d708bceac7c
Isiah Malit <isiah.malit@publicissapient.com>
2023-01-31 09:25:09 -0500

    pushing project to remote

---
+++ RecipeFinder/RecipeFinder/RecipeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeFinderApp.swift
+//  RecipeFinder
+//
+//  Created by Isiah Marie Ramos Malit on 2023-01-31.
+//
+
+import SwiftUI
+
+@main
+struct RecipeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
